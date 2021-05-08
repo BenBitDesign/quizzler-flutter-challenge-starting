15cd3728f93c213ef582ea5b386f07ac78fca3de
Ben Meulemans <ben@bitdesign.be>
2021-05-08 21:59:02 +0300

    expansionpanel test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
-import 'quiz_brain.dart';
-
-QuizBrain quizBrain = QuizBrain();
-
-void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
   @override
@@ -15,7 +9,7 @@ class Quizzler extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
+            child: StfulPage(),
           ),
         ),
       ),
@@ -23,103 +17,34 @@ class Quizzler extends StatelessWidget {
   }
 }
 
-class QuizPage extends StatefulWidget {
+class StfulPage extends StatefulWidget {
   @override
-  _QuizPageState createState() => _QuizPageState();
+  _StfulPageState createState() => _StfulPageState();
 }
 
-class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [];
-
-  void checkAnswer(bool userPickedAnswer) {
-    bool correctAnswer = quizBrain.getCorrectAnswer();
-
-    setState(() {
-      if (quizBrain.isFinished()) {
-        Alert(context: context, title: "Done", desc: "End reached.").show();
-        quizBrain.resetQuiz();
-        scoreKeeper.clear();
-        return;
-      }
-
-      if (userPickedAnswer == correctAnswer) {
-        scoreKeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
-      } else {
-        scoreKeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
-      }
-      quizBrain.nextQuestion();
-    });
-  }
-
+class _StfulPageState extends State<StfulPage> {
   @override
   Widget build(BuildContext context) {
+    List<bool> _isOpen = [false];
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                quizBrain.getQuestionText(),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                //The user picked true.
-                checkAnswer(true);
+        ExpansionPanelList(
+          children: [
+            ExpansionPanel(
+              headerBuilder: (context, isOpen) {
+                return Text('HI');
               },
+              body: Text('now open'),
+              isExpanded: _isOpen[0],
             ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-                checkAnswer(false);
-              },
-            ),
-          ),
-        ),
-        Row(
-          children: scoreKeeper,
+          ],
+          expansionCallback: (i, isOpen) {
+            _isOpen[i] = !isOpen;
+            setState(() {});
+          },
         )
       ],
     );
